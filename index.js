84ad17ff75a212a59075636e459a8d8a1e33e168
mehtabhussain12 <mehtabmughal688@gmail.com>
2025-06-24 12:50:08 +0500

    push

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-var Todo = ["karachi", "Lahore", "Islamabad"]
+var Todo = getFromLocalStorage() || ["karachi", "lahore", "islamabad", "peshawar", "quetta"];
 var list = document.getElementById("list")
 var inputbox = document.getElementById("input-box")
+
+
+
+// Function for showing the welcome message 
 function start() {
 
     var namebox = document.getElementById("name-box")
@@ -23,21 +27,21 @@ function start() {
 // Function for showing Item
 
 function ShowList() {
-    list.innerHTML = ""
+    var datafromstorage = getFromLocalStorage(); 
+    list.innerHTML = "";
     for (var i = 0; i < Todo.length; i++) {
-        list.innerHTML += `<li>${Todo[i].toLocaleLowerCase()}  <span class=svg><svg onclick="DeleteItem(${i})" height="20px" width="20px" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">Add commentMore actions
-  <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-</svg>
- </span> 
- 
-<span onclick="EditItem(${i})" class="svg">
- <svg xmlns="http://www.w3.org/2000/svg" fill="none"  viewBox="0 0 24 24" height="20px" stroke-width="1.5" stroke="currentColor" class="size-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
-</svg>
-<span/></li>`
-
+        list.innerHTML += `<li>${Todo[i].toLowerCase()}  
+        <span class=svg>
+            <svg onclick="DeleteItem(${i})" height="20px" width="20px" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" d="..."/>
+            </svg>
+        </span> 
+        <span onclick="EditItem(${i})" class="svg">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none"  viewBox="0 0 24 24" height="20px" stroke-width="1.5" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" d="..."/>
+            </svg>
+        </span></li>`;
     }
-
 }
 
 
@@ -53,7 +57,7 @@ function AddList() {
     }
     // condition if input box already exists in the list
     for (var i = 0; i < Todo.length; i++) {
-        if (Todo[i].toLocaleLowerCase() == inputbox.value.toLocaleLowerCase()) {
+        if (Todo[i].toLowerCase() == inputbox.value.toLowerCase()) {
             console.log("this item already exists")
             alert("this item already exists")
             list.innerHTML = ""
@@ -64,10 +68,11 @@ function AddList() {
         }
     }
     Todo.push(inputbox.value)
-    // list.innerHTML = ""
+    saveToLocalStorage(Todo)
     inputbox.value = ""
     inputbox.focus()
     ShowList()
+    
 
 }
 //  funtion for deleting  item
@@ -84,3 +89,15 @@ function EditItem(index) {
     ShowList()
 }
 
+
+
+function saveToLocalStorage() {
+    localStorage.setItem("todo", JSON.stringify(Todo))
+    console.log("Saved to local storage", Todo)
+}
+
+
+function getFromLocalStorage() {
+    var data = localStorage.getItem("todo")
+    console.log("Data from local storage", data)
+}
